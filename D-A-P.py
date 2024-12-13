86e39b682b3ccefe2f7cf48ef953dc04fd22102f
dakshsinghrajput <95866403+dakshsinghrajput@users.noreply.github.com>
2024-12-13 20:32:38 +0530

    Update D-A-P.py
    By Daksh Singh Rajput

--- D-A-P.py
+++ D-A-P.py
@@ -1,2 +1,17 @@
+print("hello world of D-A-P")
+import random
+#here you can se code of some unique game which are two player
+print("You can play the multiplayer games available here")
+print("Number Game")
 while True:
-      print("hello world of D-A-P")
+    a=random.randrange(1,9)
+    P1=int(input("enter any number that you think computer have chossen you are player 1"))
+    if(P1==a):
+        print("Player1 pricited computers number",a)
+        break
+    P1=int(input("enter any number that you think computer have chossen you are player 2"))
+    if(P1==a):
+        print("Player2 pricited computers number",a)
+        break
+    elif(P1==P2):
+        print("")
